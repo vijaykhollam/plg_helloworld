b07a2562debd9c3bba62a078421a9a558fec0ca7
Vijay Khollam <vijay_k@tekditechnologies.com>
2024-11-06 19:14:33 +0530

    Description of changes made

--- plg_helloworld/helloworld.php
+++ plg_helloworld/helloworld.php
@@ -9,24 +9,29 @@ class PlgSystemHelloworld extends CMSPlugin
 {
     public function onAfterInitialise()
     {
-        // Get the current hour
-        $hour = (int) date('H');
-        $greeting = '';
+		// Get the current hour in 24-hour format (0-23)
+		$hour = (int) date('H');
+		$greeting = '';
 
-        // Determine part of the day and set greeting message
-        if ($hour < 12) {
-            $greeting = 'Good morning!';
-        } elseif ($hour < 18) {
-            $greeting = 'Good afternoon!';
-        } else {
-            $greeting = 'Good evening!';
-        }
+		// Determine the appropriate greeting message based on the time of day
+		if ($hour < 12) {
+			// Morning: Set greeting to "Good morning!"
+			$greeting = 'Good morning!';
+		} elseif ($hour < 18) {
+			// Afternoon: Set greeting to "Good afternoon!"
+			$greeting = 'Good afternoon!';
+		} else {
+			// Evening: Set greeting to "Good evening!"
+			$greeting = 'Good evening!';
+		}
 
-        // Log the greeting message
-        Log::add($greeting . ' Hello from the HelloWorld Plugin.', Log::INFO, 'plg_system_helloworld');
+		// Log the selected greeting message with a custom plugin identifier
+		Log::add($greeting . ' Hello from the HelloWorld Plugin.', Log::INFO, 'plg_system_helloworld');
 
-        // Example string manipulation
-        $message = str_replace('Hello', 'Hi', 'Hello from Joomla Plugin');
-        Log::add('Modified Message: ' . $message, Log::DEBUG, 'plg_system_helloworld');
+		// Example of string manipulation: replace "Hello" with "Hi" in a sample message
+		$message = str_replace('Hello', 'Hi', 'Hello from Joomla Plugin');
+		
+		// Log the modified message at DEBUG level for troubleshooting
+		Log::add('Modified Message: ' . $message, Log::DEBUG, 'plg_system_helloworld');
     }
 }
