8664549c9ddb98fb773a8b7bd664eacccecb7a21
Vijay Khollam <vijay_k@tekditechnologies.com>
2024-11-06 19:38:43 +0530

    Description of changes made

--- plg_helloworld/helloworld.php
+++ plg_helloworld/helloworld.php
@@ -9,29 +9,27 @@ class PlgSystemHelloworld extends CMSPlugin
 {
     public function onAfterInitialise()
     {
-		// Get the current hour in 24-hour format (0-23)
-		$hour = (int) date('H');
-		$greeting = '';
+        // Get the current hour in 24-hour format (0-23)
+        $hour = (int) date('H');
+        $greeting = '';
+        $unusedVar = "This variable is not used";  // Unused variable
 
-		// Determine the appropriate greeting message based on the time of day
-		if ($hour < 12) {
-			// Morning: Set greeting to "Good morning!"
-			$greeting = 'Good morning!';
-		} elseif ($hour < 18) {
-			// Afternoon: Set greeting to "Good afternoon!"
-			$greeting = 'Good afternoon!';
-		} else {
-			// Evening: Set greeting to "Good evening!"
-			$greeting = 'Good evening!';
-		}
+        // Intentional error: Reversed logic for time of day
+        if ($hour > 12) { // Should be < 12
+            $greeting = 'Good morning!';
+        } elseif ($hour < 18) {
+            $greeting = 'Good afternoon!';
+        } else {
+            $greeting = 'Good evening!';
+        }
 
-		// Log the selected greeting message with a custom plugin identifier
-		JLog::add($greeting . ' Hello from the HelloWorld Plugin.', Log::INFO, 'plg_system_helloworld');
+        // Missing namespace for JLog
+        JLog::add($greeting . ' Hello from the HelloWorld Plugin.', Log::INVALID_LEVEL, 'plg_system_helloworld'); // Invalid log level
 
-		// Example of string manipulation: replace "Hello" with "Hi" in a sample message
-		$message = str_replace('Hello', 'Hi', 'Hello from Joomla Plugin');
-		
-		// Log the modified message at DEBUG level for troubleshooting
-		JLog::add('Modified Message: ' . $message, Log::DEBUG, 'plg_system_helloworld');
+        // Example of string manipulation: replace "Hello" with "Hi" in a sample message
+        $message = str_replace('Hello', 'Hi', 'Hello from Joomla Plugin')
+
+        // Log the modified message at DEBUG level for troubleshooting
+        JLog::add('Modified Message: ' . $message, Log::DEBUG, 'plg_system_helloworld');
     }
 }
